0f1c83f4c789d337e0e1bf8c2f78018b3b50d149
Vitali.D <damastoivitali@gmail.com>
2020-02-04 23:18:51 +0300

    django project

--- parser/parser.py
+++ parser/parser.py
@@ -34,22 +34,66 @@ class Source:
     def getLinks(self, url):
         page = requests.get(url)
         soup = BeautifulSoup(page.content, 'html.parser')
-        links = {}
+        links = []
+
+        links.append({
+            'name': self.name
+        })
+
         for link in soup.findAll('a', attrs={'href': re.compile("")}):
-            links.update({re.sub('\s+', ' ', link.text.strip().replace(u'\xa0', u' ').replace('\n', ' ')): (link.get('href') if 'http' in link.get('href') else self.name + link.get('href'))})
+            # links.update({re.sub('\s+', ' ', link.text.strip().replace(u'\xa0', u' ').replace('\n', ' ')): (link.get('href') if 'http' in link.get('href') else self.name + link.get('href'))})
                                 # delete multiple spaces               replace symbols                replace \n to space     add domain to relative links
+
+            title = re.sub('/s+', ' ', link.text.strip().replace(u'\xa0', u' ').replace('\n', ' '))
+            address = link.get('href') if 'http' in link.get('href') else self.name + link.get('href')
+
+            if 'tut.by' in url:
+                if len(title) > 2 and '/news/' in address:
+                    links.append({
+                        'title': title,
+                        'url': address
+                    })
+            elif len(title) > 2:
+                links.append({
+                    'title': title,
+                    'url': address
+                })
 
         return links
 
 
+class tutLinks(Source):
+    def getLinks(self, url):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        links = []
+
+        links.append({
+            'name': self.name
+        })
+        for link in soup.findAll('a', attrs={'href': re.compile("")}):
+
+            title = re.sub('/s+', ' ', link.text.strip().replace(u'\xa0', u' ').replace('\n', ' '))
+            address = link.get('href') if 'http' in link.get('href') else self.name + link.get('href')
+
+            if len(title) > 2 and '/news/' in address:
+                links.append({
+                    'title': title,
+                    'url': address
+                })
+        return links
+
+
+tut = tutLinks('https://tut.by')
+pprint.pprint(tut.getLinks('https://tut.by'))
+
 nPlusOne = Source('https://nplus1.ru')
 esquire = Source('https://esquire.ru')
-tut = Source('https://tut.by')
+# tut = Source('https://tut.by')
 
-# pprint.pprint(nPlusOne.getLinks('https://nplus1.ru/'))
+#pprint.pprint(nPlusOne.getLinks('https://nplus1.ru/'))
 # pprint.pprint(esquire.getLinks('https://esquire.ru'))
 # pprint.pprint(tut.getLinks('https://tut.by'))
 
 
-# print(jsjs)
 
